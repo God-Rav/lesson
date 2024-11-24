66fb84e558514e4a298176d30085dacff11fde07
God-Rav <ravka2077@gmail.com>
2024-11-24 05:40:23 +0300

    Add files via upload

---
+++ PythonUrban.py
@@ -0,0 +1,19 @@
+print("hello world")
+
+#"1st program"
+print(9 ** 0.5 * 5)  # Ответ: 15.0
+
+#"2nd program"
+print(9.99 > 9.98 and 1000 != 1000.1) # Ответ: True
+
+# "3rd program"
+print(2 * 2 + 2) # Ответ: 6
+print(2 * (2 + 2)) # Ответ: 8
+
+print(2 * 2 + 2 == 2 * (2 + 2)) # Ответ: False
+
+# "4th program"
+print(int(float('123.456') * 10 % 10)) # Ответ: 4
+
+
+
