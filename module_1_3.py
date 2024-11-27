9721c37220b94c35779be873a42d9bc6585129b4
God-Rav <ravka2077@gmail.com>
2024-11-27 17:07:10 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,14 @@
+name = 'Ravl'
+print('Name:', name)
+age = 28
+print('Age:', age)
+age = 10
+print('New age:', age)
+is_student = True
+print('Is_student:', is_student)
+
+# Результат:
+# Name: Ravl
+# Age: 28
+# New age: 10
+# Is_student: True
