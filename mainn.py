421991144cacbd6eef5c1e758a43f69b50bf25e3
God-Rav <ravka2077@gmail.com>
2024-11-27 19:53:10 +0300

    Update mainn.py

--- mainn.py
+++ mainn.py
@@ -3,3 +3,6 @@ hours = 1.5
 course_name = 'Python'
 time_for_one_task =  hours / tasks
 print('Курс: ' + course_name + ',', 'всего задач:', str(tasks) +', затрачено часов:', str(hours) + ', среднее время выполнения', time_for_one_task, 'часа.')
+
+# Результат
+# Курс: Python, всего задач: 12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.
