635a61eb928905d6213d07351f3371150a8c128d
God-Rav <ravka2077@gmail.com>
2024-11-27 19:50:12 +0300

    Практическое задание по теме "Переменные"

---
+++ mainn.py
@@ -0,0 +1,5 @@
+tasks = 12
+hours = 1.5
+course_name = 'Python'
+time_for_one_task =  hours / tasks
+print('Курс: ' + course_name + ',', 'всего задач:', str(tasks) +', затрачено часов:', str(hours) + ', среднее время выполнения', time_for_one_task, 'часа.')
