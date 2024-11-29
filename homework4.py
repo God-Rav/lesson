0e6bc705b3ef55f5fef965cbaac8781b2451d5ef
God-Rav <ravka2077@gmail.com>
2024-11-29 18:43:41 +0300

    Update homework4.py

--- homework4.py
+++ homework4.py
@@ -3,11 +3,11 @@ print(example[0])
 print(example[-1])
 print(example[4:])
 print(example[::-1])
-print(example[0:7:2])
+print(example[1:7:2])
 
 # result:
 # С
 # й
 # рай
 # йарумаС
-# Смрй
+# ауа
