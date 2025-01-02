d80cb61f7eed5f4cfb424275f6bc42a602434101
God-Rav <ravka2077@gmail.com>
2025-01-02 09:07:57 +0300

    Самостоятельная работа по уроку "Рекурсия"

---
+++ module_3.5.py
@@ -0,0 +1,23 @@
+# Самостоятельная работа по уроку "Рекурсия"
+
+def get_multiplied_digits(number):
+    str_number = str(number)
+    first = int(str_number[0])
+    if '0' in str_number:
+        str_number = str_number.replace('0', '')
+
+    if len(str_number) > 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
+
+    else:
+        return first
+
+
+result = get_multiplied_digits(40203)
+print(result)
+result2 = get_multiplied_digits(402030)
+print(result2)
+
+# result
+# 24
+# 24
